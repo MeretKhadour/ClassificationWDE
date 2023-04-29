0616bc6b433b4096d3aa4e4ae6d0152bb2bbda91
MeretKhadour <132152353+MeretKhadour@users.noreply.github.com>
2023-04-30 06:23:36 +0700

    Update meret-app.py

--- MeretWDE/meret-app.py
+++ MeretWDE/meret-app.py
@@ -182,7 +182,7 @@ if DisplayCategoricalNB_button:
 
 st.write(''' 
 ### Show Decistion Tree  ''')
-image = Image.open('model-building\decistion_tree.png')
+image = Image.open('https://github.com/MeretKhadour/ClassificationWDE/blob/main/MeretWDE/model-building/decistion_tree.png')
 st.image(image, caption='Decistion Tree')
 
 

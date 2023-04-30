1bd8a5220db3c9d7c7266c5af18d45899519c64d
MeretKhadour <132152353+MeretKhadour@users.noreply.github.com>
2023-04-30 07:28:56 +0700

    Update app.py

--- MeretWDE/app.py
+++ MeretWDE/app.py
@@ -67,7 +67,7 @@ X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
 ## Reads in saved classification model
  
 
-model_CategoricalNB_clf = pickle.Unpickler.read('model_LogisticRegression.pkl')
+model_CategoricalNB_clf = pickle.Unpickler.load(open('model_LogisticRegression.pkl'))
 
  
 model_decision_tree_clf = []
